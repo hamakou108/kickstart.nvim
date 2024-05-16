767c6a1d67a9aad32277a956fb696c526768a189
hamakou108 <hamakou108@gmail.com>
2024-05-17 08:16:15 +0900

    feat: mdx filetype

--- init.lua
+++ init.lua
@@ -210,6 +210,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Set the filetype for .mdx files to 'markdown'
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = { '*.mdx' },
+  command = 'set filetype=markdown',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
